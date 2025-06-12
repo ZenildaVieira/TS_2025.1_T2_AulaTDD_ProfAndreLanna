ece63b9504a53bc9c72ba50298b59bf2f9810c57
Zenilda Pedrosa Vieira <101183963+ZenildaVieira@users.noreply.github.com>
2025-06-12 18:55:10 -0300

    Create CadastrarDeducoesTest.java

---
+++ CalculoIRPF/src/tst/CadastrarDeducoesTest.java
@@ -0,0 +1,25 @@
+package tst;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import app.IRPF;
+
+public class CadastrarDeducoesTest {
+
+    private IRPF irpf;
+
+    @Before
+    public void setup() {
+        irpf = new IRPF();
+    }
+
+    @Test
+    public void testCadastrarPrevidenciaOficial1() {
+        irpf.cadastrarPrevidenciaOficial(700f);
+        assertEquals(700f, irpf.getPrevidenciaOficial(), 0);
+    }
+    
+}
