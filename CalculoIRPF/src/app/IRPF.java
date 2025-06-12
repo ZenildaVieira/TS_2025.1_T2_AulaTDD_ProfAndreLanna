06d3b112f2bd7eeda3f45c5582eb2d2b26a76f69
Zenilda Pedrosa Vieira <101183963+ZenildaVieira@users.noreply.github.com>
2025-06-12 19:12:04 -0300

    Update IRPF.java - cadastrar dependente

--- CalculoIRPF/src/app/IRPF.java
+++ CalculoIRPF/src/app/IRPF.java
@@ -4,6 +4,7 @@ public class IRPF {
 	
 	private float salario;
 	private float contribuicaoPrevidenciaria;
+	private String[] dependentes;
     
 	public void cadastrarSalario(float salario) {
 		this.salario += salario;
@@ -14,11 +15,26 @@ public class IRPF {
 	}
 
 	public void cadastrarPrevidenciaOficial(float contribuicaoPrevidenciaria) {
-        	this.contribuicaoPrevidenciaria = contribuicaoPrevidenciaria;
-    	}
+        this.contribuicaoPrevidenciaria = contribuicaoPrevidenciaria;
+    }
 
-    	public float getPrevidenciaOficial() {
-        	return this.contribuicaoPrevidenciaria;
-    	}
+    public float getPrevidenciaOficial() {
+        return this.contribuicaoPrevidenciaria;
+    }
 
+    public void cadastrarDependente(String nome) {
+        // Criar um nova posicao no vetor de dependentes
+        String[] temp = new String[dependentes.length + 1];
+        for (int i = 0; i < dependentes.length; i++) {
+            temp[i] = dependentes[i];
+        }
+
+        // Alocar novo dependente no vetor
+        temp[dependentes.length] = nome;
+        
+        // atualizar referencias do vetor de dependentes
+        dependentes = temp;
+    }
+    
 }
+
