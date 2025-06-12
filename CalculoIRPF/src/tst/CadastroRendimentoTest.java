6af3e7e84a1a64fb47090c1280af1d36c8677a15
Zenilda Pedrosa Vieira <101183963+ZenildaVieira@users.noreply.github.com>
2025-06-12 18:48:59 -0300

    Update CadastroRendimentoTest.java - triangulacao

--- CalculoIRPF/src/tst/CadastroRendimentoTest.java
+++ CalculoIRPF/src/tst/CadastroRendimentoTest.java
@@ -18,14 +18,20 @@ public class CadastroRendimentoTest {
     public void testCadastrarSalario() {
         irpf.cadastrarSalario(5000f);
         assertEquals(5000f, irpf.getTotalSalario(), 0);
-        // fail("Not yet implemented");
     }
 
     @Test
     public void testCadastrarSalario2() {
         irpf.cadastrarSalario(6000f);
         assertEquals(6000f, irpf.getTotalSalario(), 0);
-        // fail("Not yet implemented");
     }
+
+    @Test
+    public void testCadastrarSalario3() {
+        irpf.cadastrarSalario(5000f);
+        irpf.cadastrarSalario(6000f);
+        assertEquals(11000f, irpf.getTotalSalario(), 0);
+    }
+
 }
 
