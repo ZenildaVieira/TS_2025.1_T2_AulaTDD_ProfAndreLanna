ddb905baec024c3223661c40a999cd4f715f6111
Zenilda Pedrosa Vieira <101183963+ZenildaVieira@users.noreply.github.com>
2025-06-12 18:58:32 -0300

    Update IRPF.java - previdencia oficial

--- CalculoIRPF/src/app/IRPF.java
+++ CalculoIRPF/src/app/IRPF.java
@@ -3,13 +3,22 @@ package app;
 public class IRPF {
 	
 	private float salario;
-	
+	private float contribuicaoPrevidenciaria;
+    
 	public void cadastrarSalario(float salario) {
 		this.salario += salario;
 	}
 	
 	public float getTotalSalario() {		
-		return this.salario; 
+		return this.salario;
 	}
+
+	public void cadastrarPrevidenciaOficial(float contribuicaoPrevidenciaria) {
+        	this.contribuicaoPrevidenciaria = contribuicaoPrevidenciaria;
+    	}
+
+    	public float getPrevidenciaOficial() {
+        	return this.contribuicaoPrevidenciaria;
+    	}
 
 }
